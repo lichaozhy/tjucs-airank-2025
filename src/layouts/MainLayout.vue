5c13c6507faada7ca09f685100f77d1d35bcc157
KawOwl <kawowl@outlook.com>
2025-05-31 18:43:16 +0800

    [ADDED] fix footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
 	<q-layout view="lHh Lpr lFf">
 		<q-header elevated>
 			<q-toolbar>
-				<q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+				<!-- <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" /> -->
 
 				<!-- <q-toolbar-title> TJU CS AIRank </q-toolbar-title> -->
 				<q-toolbar-title>
@@ -16,15 +16,25 @@
 		<q-page-container>
 			<router-view />
 		</q-page-container>
+
+		<q-footer>
+			<q-toolbar>
+				<q-toolbar-title class="flex">
+					<span>© 2025 Tianjin University All Rights Reserved</span>
+					<q-separator class="q-mx-md bg-white" vertical />
+					<span>Tianjin University Deep Reinforcement Learning Laboratory</span>
+				</q-toolbar-title>
+			</q-toolbar>
+		</q-footer>
 	</q-layout>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+// import { ref } from 'vue';
 
-const leftDrawerOpen = ref(false);
+// const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer() {
-	leftDrawerOpen.value = !leftDrawerOpen.value;
-}
+// function toggleLeftDrawer() {
+// 	leftDrawerOpen.value = !leftDrawerOpen.value;
+// }
 </script>
