e8c8d0f5841ff524af3126a367420c387cf92172
chaoslee <chaos2lee@qq.com>
2025-06-13 15:12:06 +0800

    [FIXED] Page::ModelDetail: property chips

--- src/spec/Model.ts
+++ src/spec/Model.ts
@@ -10,14 +10,20 @@ export const Schema = z.object({
 	size: z.array(z.uint32()).optional(),
 	author: z.array(z.string()).optional(),
 	release: z.object({
-		year: z.uint32().optional(),
+		year: z.uint32(),
 		month: z.uint32().optional(),
 		date: z.uint32().optional(),
 	}).optional(),
-	opensource: z.boolean().nullable().optional(),
+	opensource: z.boolean().optional(),
 	qa: z.array(z.enum(['2D', '3D'])).optional(),
-	navigation: z.boolean().nullable().optional(),
-	taskPlanning: z.boolean().nullable().optional(),
+	navigation: z.boolean().optional(),
+	taskPlanning: z.boolean().optional(),
+	website: z.url().optional(),
+	dimension: z.array(z.enum(['2D', '3D'])).optional(),
+	reason: z.boolean().optional(),
+	imageVideo: z.enum([
+		'both', 'image/video', 'Image', 'Video', 'Point Clouds',
+	]).optional(),
 });
 
 export type Type = z.infer<typeof Schema>
