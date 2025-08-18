5e6f67a942aa6e57338617253edbfaeff4aed48f
chaoslee <chaos2lee@qq.com>
2025-08-18 09:24:40 +0800

    [FIXED] Component::ModelFilter::MainButton: using dialog instead of dropdown

--- src/components/ModelFilter/MainButton.vue
+++ src/components/ModelFilter/MainButton.vue
@@ -1,68 +1,74 @@
 <template>
-	<q-btn-dropdown
+	<q-btn
 		dark
 		text-color="white"
 		dropdown-icon="filter_alt"
-		outline
+		color="indigo-10"
 		square
 		content-class="no-border-radius"
 		transition-show="none"
+		icon-right="filter_alt"
+		@click="showing = !showing"
+		unelevated
 	>
-		<template #label>
+		<div class="q-mr-sm">
 			<span>{{ filteredList.length }}</span>
 			<span class="q-mx-sm">/</span>
 			<span>{{ props.models.length }}</span>
-		</template>
-
-		<q-list
-			style="width: 50vw; max-width: 800px; min-width: 300px;"
-			dense
-		>
-			<q-item-label
-				header
-				class="text-capitalize text-black text-weight-bold"
-				>Filter Options</q-item-label
-			>
-			<q-separator />
-			<template
-				v-for="(item) in itemList"
-				:key="item.name"
-			>
+		</div>
+	</q-btn>
+	<q-dialog
+		v-model="showing"
+		square
+	>
+		<q-card>
+			<q-list dense>
 				<q-item-label
 					header
-					caption
-					class="text-capitalize text-weight-bold q-py-sm"
-					>{{ item.name }}</q-item-label
+					class="text-capitalize text-black text-weight-bold"
+					>Filter Options</q-item-label
 				>
-				<q-item>
-					<app-item-enum
-						v-if="item.type === 'enum'"
-						v-bind="item.props"
-						v-model:reset="item.modelReset"
-						v-model:filter="filters[item.name]"
-						v-model="item.modelValue"
-					></app-item-enum>
-					<app-item-range
-						v-if="item.type === 'range'"
-						v-bind="item.props"
-						v-model:reset="item.modelReset"
-						v-model:filter="filters[item.name]"
-						v-model="item.modelValue"
-					></app-item-range>
-				</q-item>
-				<q-separator spaced />
-			</template>
-		</q-list>
-		<q-toolbar>
-			<q-space />
-			<q-btn
-				flat
-				square
-				label="reset"
-				@click="resetAll"
-			/>
-		</q-toolbar>
-	</q-btn-dropdown>
+				<q-separator />
+				<template
+					v-for="item in itemList"
+					:key="item.name"
+				>
+					<q-item-label
+						header
+						caption
+						class="text-capitalize text-weight-bold q-py-sm"
+						>{{ item.name }}</q-item-label
+					>
+					<q-item>
+						<app-item-enum
+							v-if="item.type === 'enum'"
+							v-bind="item.props"
+							v-model:reset="item.modelReset"
+							v-model:filter="filters[item.name]"
+							v-model="item.modelValue"
+						></app-item-enum>
+						<app-item-range
+							v-if="item.type === 'range'"
+							v-bind="item.props"
+							v-model:reset="item.modelReset"
+							v-model:filter="filters[item.name]"
+							v-model="item.modelValue"
+						></app-item-range>
+					</q-item>
+					<q-separator spaced />
+				</template>
+			</q-list>
+			<q-toolbar>
+				<q-space />
+				<q-btn
+					flat
+					square
+					label="reset"
+					@click="resetAll"
+				/>
+			</q-toolbar>
+		</q-card>
+	</q-dialog>
 </template>
 
 <script setup lang="ts">
@@ -99,17 +105,17 @@ interface EnumItem extends BaseItem {
 	type: 'enum';
 	modelValue: Record<string, boolean>;
 	props: {
-		values: string[]
-	}
+		values: string[];
+	};
 }
 
 interface RangeItem extends BaseItem {
 	type: 'range';
-	modelValue: { min: number, max: number };
+	modelValue: { min: number; max: number };
 	props: {
 		min: number;
 		max: number;
-	}
+	};
 }
 
 const DEFAULT = {
@@ -117,6 +123,7 @@ const DEFAULT = {
 	FILTER: () => true,
 };
 
+const showing = ref<boolean>(false);
 const modelDataRecord = ref<Record<string, Data.Model>>({});
 const itemList = ref<(EnumItem | RangeItem)[]>([]);
 
@@ -193,81 +200,86 @@ const filteredList = computed<ModelData[]>(() => {
 	});
 });
 
-const Record = (list: string[]) => Object.fromEntries(list.map(v => [v, true]));
+const Record = (list: string[]) =>
+	Object.fromEntries(list.map((v) => [v, true]));
 
-watch(filteredList, list => emit('filtered', list), { immediate: true });
+watch(filteredList, (list) => emit('filtered', list), { immediate: true });
 
-watch(() => props.models, async (modelList) => {
-	const idList = modelList.map(({ id }) => id);
-	const modelFetchingList = idList.map(id => Backend.API.Model(id).get());
-	const modelDataList = await Promise.all(modelFetchingList);
-	const _group = await Backend.API.Model.PropertyRecord.query(...idList);
-	const visionList = Object.keys(_group.vision);
-	const languageList = Object.keys(_group.language);
-	const authorList = Object.keys(_group.author);
-	const sizeList = Object.keys(_group.size).map(Number);
-	const yearList = Object.keys(_group.year).map(Number);
+watch(
+	() => props.models,
+	async (modelList) => {
+		const idList = modelList.map(({ id }) => id);
+		const modelFetchingList = idList.map((id) => Backend.API.Model(id).get());
+		const modelDataList = await Promise.all(modelFetchingList);
+		const _group = await Backend.API.Model.PropertyRecord.query(...idList);
+		const visionList = Object.keys(_group.vision);
+		const languageList = Object.keys(_group.language);
+		const authorList = Object.keys(_group.author);
+		const sizeList = Object.keys(_group.size).map(Number);
+		const yearList = Object.keys(_group.year).map(Number);
 
-	itemList.value = [
-		{
-			name: 'vision',
-			type: 'enum',
-			modelReset: DEFAULT.RESET,
-			modelValue: Record(visionList),
-			props: {
-				values: visionList,
+		itemList.value = [
+			{
+				name: 'vision',
+				type: 'enum',
+				modelReset: DEFAULT.RESET,
+				modelValue: Record(visionList),
+				props: {
+					values: visionList,
+				},
 			},
-		},
-		{
-			name: 'language',
-			type: 'enum',
-			modelReset: DEFAULT.RESET,
-			modelValue: Record(languageList),
-			props: {
-				values: languageList,
+			{
+				name: 'language',
+				type: 'enum',
+				modelReset: DEFAULT.RESET,
+				modelValue: Record(languageList),
+				props: {
+					values: languageList,
+				},
 			},
-		},
-		{
-			name: 'author',
-			type: 'enum',
-			modelReset: DEFAULT.RESET,
-			modelValue: Record(authorList),
-			props: {
-				values: authorList,
+			{
+				name: 'author',
+				type: 'enum',
+				modelReset: DEFAULT.RESET,
+				modelValue: Record(authorList),
+				props: {
+					values: authorList,
+				},
 			},
-		},
-		{
-			name: 'size',
-			type: 'range',
-			modelReset: DEFAULT.RESET,
-			modelValue: {
-				min: Math.min(...sizeList),
-				max: Math.max(...sizeList),
+			{
+				name: 'size',
+				type: 'range',
+				modelReset: DEFAULT.RESET,
+				modelValue: {
+					min: Math.min(...sizeList),
+					max: Math.max(...sizeList),
+				},
+				props: {
+					min: Math.min(...sizeList),
+					max: Math.max(...sizeList),
+				},
 			},
-			props: {
-				min: Math.min(...sizeList),
-				max: Math.max(...sizeList),
+			{
+				name: 'year',
+				type: 'range',
+				modelReset: DEFAULT.RESET,
+				modelValue: {
+					min: Math.min(...yearList),
+					max: Math.max(...yearList),
+				},
+				props: {
+					min: Math.min(...yearList),
+					max: Math.max(...yearList),
+				},
 			},
-		},
-		{
-			name: 'year',
-			type: 'range',
-			modelReset: DEFAULT.RESET,
-			modelValue: {
-				min: Math.min(...yearList),
-				max: Math.max(...yearList),
-			},
-			props: {
-				min: Math.min(...yearList),
-				max: Math.max(...yearList),
-			},
-		},
-	];
+		];
 
-	const modelDataEntries = modelDataList.map(({ id, ...data }) => [id, data]);
+		const modelDataEntries = modelDataList.map(({ id, ...data }) => [id, data]);
 
-	modelDataRecord.value = Object.fromEntries(modelDataEntries);
-}, { immediate: true });
+		modelDataRecord.value = Object.fromEntries(modelDataEntries);
+	},
+	{ immediate: true },
+);
 
 defineOptions({ name: 'AppModelFilter' });
 </script>
