316333c04aa759edf7a059d20da68618a416e692
chaoslee <chaos2lee@qq.com>
2025-12-07 19:03:16 +0800

    [ADDED] vercel speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
 	<router-view />
 	<analytics></analytics>
+	<speed-insights></speed-insights>
 </template>
 
 <script setup lang="ts">
 import 'temml/dist/Temml-Local.css';
 import './css/app.scss';
 import { Analytics } from '@vercel/analytics/vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
