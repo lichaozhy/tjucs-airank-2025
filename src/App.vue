ce0b9dc435681283b91fb4d1e30bfadeb4af1959
chaoslee <chaos2lee@qq.com>
2025-12-07 14:33:25 +0800

    [ADDED] vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
 	<router-view />
+	<analytics></analytics>
 </template>
 
 <script setup lang="ts">
 import 'temml/dist/Temml-Local.css';
 import './css/app.scss';
+import { Analytics } from '@vercel/analytics/vue';
 </script>
